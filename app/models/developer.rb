ba64df49f9ec8c3dccdd410cb7c7a2fec5ec82bd
suzuki_tomoko <suzuki_tomoko@sirok.co.jp>
2018-04-01 12:44:32 +0700

    Add relationship between Developer and ProgrammingLanguage

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,2 +1,4 @@
 class Developer < ApplicationRecord
+  has_many :developer_programming_languages, dependent: :destroy
+  has_many :programming_languages, through: :developer_programming_languages
 end

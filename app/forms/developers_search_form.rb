8c5dba168eaa9e73cbb86bf43b41c5d7686818ad
suzuki_tomoko <suzuki_tomoko@sirok.co.jp>
2018-04-03 19:54:38 +0700

    Change sql

--- app/forms/developers_search_form.rb
+++ app/forms/developers_search_form.rb
@@ -9,9 +9,9 @@ class DevelopersSearchForm
   PARAMS = [:programming_language_id, :language_id].freeze
 
   def search
-    query = Developer.all
-    query = query.includes(:developer_programming_languages).where(developer_programming_languages: { programming_language_id: programming_language_id }) if programming_language_id.present?
-    query = query.includes(:developer_languages).where(developer_languages: { language_id: language_id }) if language_id.present?
+    query = Developer.all.includes(:developer_programming_languages, :programming_languages, :developer_languages, :languages)
+    query = query.where(developer_programming_languages: { programming_language_id: programming_language_id }) if programming_language_id.present?
+    query = query.where(developer_languages: { language_id: language_id }) if language_id.present?
     query.order(created_at: :desc)
   end
 

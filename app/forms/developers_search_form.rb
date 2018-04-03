47d4fb84ff04dbd6cd9d463b840500346f4f08ab
suzuki_tomoko <suzuki_tomoko@sirok.co.jp>
2018-04-03 19:46:12 +0700

    Add language

--- app/forms/developers_search_form.rb
+++ app/forms/developers_search_form.rb
@@ -4,12 +4,14 @@ class DevelopersSearchForm
   include ActiveRecord::Sanitization::ClassMethods
 
   attribute :programming_language_id, String
+  attribute :language_id, String
 
-  PARAMS = [:programming_language_id].freeze
+  PARAMS = [:programming_language_id, :language_id].freeze
 
   def search
     query = Developer.all
     query = query.includes(:developer_programming_languages).where(developer_programming_languages: { programming_language_id: programming_language_id }) if programming_language_id.present?
+    query = query.includes(:developer_languages).where(developer_languages: { language_id: language_id }) if language_id.present?
     query.order(created_at: :desc)
   end
 
